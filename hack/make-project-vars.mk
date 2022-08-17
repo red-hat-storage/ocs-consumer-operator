b95d723af9eab26f0b139d78043ebdc64858105e
Jose A. Rivera <jarrpa@redhat.com>
2022-08-17 11:02:30 -0500

    repo: initial commit
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

---
+++ hack/make-project-vars.mk
@@ -0,0 +1,11 @@
+PROJECT_DIR := $(PWD)
+BIN_DIR := $(PROJECT_DIR)/bin
+ENVTEST_ASSETS_DIR := $(PROJECT_DIR)/testbin
+
+GOBIN ?= $(BIN_DIR)
+GOOS ?= linux
+GOARCH ?= amd64
+
+GO_LINT_IMG_LOCATION ?= golangci/golangci-lint
+GO_LINT_IMG_TAG ?= v1.45.2
+GO_LINT_IMG ?= $(GO_LINT_IMG_LOCATION):$(GO_LINT_IMG_TAG)
